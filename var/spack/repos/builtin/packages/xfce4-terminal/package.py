a146a763c4636fda4d45722c79998457a69a7263
Teague Sterling <teaguesterling@gmail.com>
2024-06-14 19:24:46 -0700

    Adding old versions
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/xfce4-terminal/package.py
+++ var/spack/repos/builtin/packages/xfce4-terminal/package.py
@@ -2,25 +2,6 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-from spack.package import *
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install xfce4-terminal
-#
-# You can edit this file again by typing:
-#
-#     spack edit xfce4-terminal
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 from spack.package import *
 
 
@@ -29,6 +10,14 @@ class Xfce4Terminal(AutotoolsPackage):
 
     homepage = "https://docs.xfce.org/apps/xfce4-terminal/start"
     url = "https://archive.xfce.org/src/apps/xfce4-terminal/1.1/xfce4-terminal-1.1.3.tar.bz2"
+    list_url = "https://archive.xfce.org/src/apps/xfce4-terminal/"
+    list_depth = 2
+
+    def url_for_version(self, version):
+        base = f"https://archive.xfce.org/src/apps/{self.name}"
+        dirname = version.up_to(2)
+        filename = f"{self.name}-{version}.tar.bz2"
+        return join_path(base, dirname, filename)
 
     maintainers("teaguesterling")
 
@@ -38,6 +27,8 @@ class Xfce4Terminal(AutotoolsPackage):
     version("1.1.2", sha256="79a85ee216502c7248e04d560adf8fef86b9d0e047f81e9ea4fe26fbda34d810")
     version("1.1.1", sha256="5ab5c9b49c00be29f0be4eee5ccfa5073b16f2456185270265a9324549080aa6")
     version("1.1.0", sha256="40823377242b9b09749f5d4a550f41d465153c235c31f34052f804d3453ad4a3")
+    version("1.0.4", sha256="78e55957af7c6fc1f283e90be33988661593a4da98383da1b0b54fdf6554baf4")
+    version("1.0.0", sha256="593b6a7bd9b18851e51854e075990109b7896a22713b5dd8b913b23f21db6576")
 
     depends_on("intltool@0.35:")
 
