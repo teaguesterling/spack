3ad193f85c7100b25b724a4c694ef45cb635083f
Teague Sterling <teaguesterling@gmail.com>
2024-06-14 18:37:56 -0700

    Adding VTE versions
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/vte/package.py
+++ var/spack/repos/builtin/packages/vte/package.py
@@ -10,12 +10,17 @@ class Vte(MesonPackage):
 
     homepage = "https://gitlab.gnome.org/GNOME/vte"
     url = "https://gitlab.gnome.org/GNOME/vte/-/archive/0.76.3/vte-0.76.3.tar.gz"
-    list_url = "https://gitlab.gnome.org/GNOME/vte/-/archive/"
-    list_depth = 2
+    list_url = "https://gitlab.gnome.org/GNOME/vte/-/tags"
+    list_depth = 1
 
     license("CC-BY-4-0", checked_by="teaguesterling")
 
     version("0.76.3", sha256="eb6d80b879914b89c22811743a5d779fa0e9c57acfa925873b521d054b809746")
+    version("0.75.92", sha256="c334eb26b6c67da8c2990eebe3c498520dad949522ba21df48295aff99285f14")
+    version("0.74.2", sha256="03a5a41c777d233341753d8ecd23c882e76f6464310bb2b8065425a1c859060a")
+    version("0.73.99", sha256="a4dedc1c8f3f62a3bff5c5da1dc2458f3f8cc30e884426f512f932aa37361e94")
+    version("0.72.4", sha256="aaee5620d654c83a3298bce89852259188fdc79c15c121aa5d316cf5a2500b3f")
+    version("0.70.6", sha256="caf4bfc8f02b633bc16658559ced98101e82da4efb80b72ff3ef7d98325e2aa8")
 
     variant("icu", default=True, description="Build with icu4u support")
     variant("gnutls", default=True, description="Build with gnutls support")
