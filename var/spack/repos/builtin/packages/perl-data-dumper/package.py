68465e313c7c5ca12042ff2f304ce8a6a5693ad7
Teague Sterling <teaguesterling@gmail.com>
2024-08-12 22:00:59 -0700

    perl-data-dumper: add libxcrypt link dep
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/perl-data-dumper/package.py
+++ var/spack/repos/builtin/packages/perl-data-dumper/package.py
@@ -13,3 +13,5 @@ class PerlDataDumper(PerlPackage):
     url = "https://cpan.metacpan.org/authors/id/X/XS/XSAWYERX/Data-Dumper-2.173.tar.gz"
 
     version("2.173", sha256="697608b39330988e519131be667ff47168aaaaf99f06bd2095d5b46ad05d76fa")
+
+    depends_on("libxcrypt", type="link")
