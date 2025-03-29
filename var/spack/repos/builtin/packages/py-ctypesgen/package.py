5eda7cd130fa25491c7fca2a5d01bc7b2c916e79
Teague Sterling <teaguesterling@gmail.com>
2025-03-29 16:28:29 -0700

    add patch for sysdefs.h
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/py-ctypesgen/package.py
+++ var/spack/repos/builtin/packages/py-ctypesgen/package.py
@@ -15,6 +15,14 @@ class PyCtypesgen(PythonPackage):
 
     version("1.1.1", sha256="deaa2d64a95d90196a2e8a689cf9b952be6f3366f81e835245354bf9dbac92f6")
 
+    # See: https://github.com/ctypesgen/ctypesgen/issues/181
+    # This fixes syntax errors when parsing /usr/include/sys/cdefs.h
+    patch(
+        "https://patch-diff.githubusercontent.com/raw/ctypesgen/ctypesgen/pull/207.patch", 
+        sha256="eacd9144f0e99df1dfdc43a3e504fc399e997f1270d56dcfc013b98529f518ce",
+        when="@:1.1.1",
+    )
+
     depends_on("c", type="build")
 
     depends_on("py-setuptools@64:", type="build")
